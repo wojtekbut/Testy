3a834acccd808d0fd4259f84c514246b647b5552
wojtekbut <wojtekbut@gmail.com>
2017-04-30 00:54:09 +0200

    przed probook

---
+++ app/src/test/java/com/example/wojtek/testy/AcceptThreadTest.java
@@ -0,0 +1,20 @@
+package com.example.wojtek.testy;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by wojtek on 30.01.17.
+ */
+public class AcceptThreadTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void writeb() throws Exception {
+
+    }
+
+}
